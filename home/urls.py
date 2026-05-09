5bcb62727ac102986b60640b429a7ad5b735de1d
Akshaj Velpula <akshajvelpula@gmail.com>
2026-05-09 08:27:19 +0000

    add Django URL routes for new SPA pages

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('faq/', views.faq, name='faq'),
-    path('faq', views.faq, name='faq_noslash'),
+    path('faq/', views.index, name='faq'),
+    path('faq', views.index, name='faq_noslash'),
+    path('team/', views.index, name='team'),
+    path('team', views.index, name='team_noslash'),
+    path('committees/', views.index, name='committees'),
+    path('committees', views.index, name='committees_noslash'),
+    path('guidelines/', views.index, name='guidelines'),
+    path('guidelines', views.index, name='guidelines_noslash'),
+    path('schedule/', views.index, name='schedule'),
+    path('schedule', views.index, name='schedule_noslash'),
 ]
