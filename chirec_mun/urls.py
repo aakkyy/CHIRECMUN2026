80931e85bc592a73b3639840759b02c23c5c1318
Akshaj Velpula <akshajvelpula@gmail.com>
2026-06-20 06:08:08 +0000

    perf: cache media files for 30 days in browser

--- chirec_mun/urls.py
+++ chirec_mun/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.decorators.cache import cache_control
 from django.views.static import serve
+
+# Cache media files for 30 days in browser
+cached_serve = cache_control(max_age=2592000, public=True, immutable=True)(serve)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    # Serve media files in both dev and production
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # Serve media files with long-lived cache headers
+    re_path(r'^media/(?P<path>.*)$', cached_serve, {'document_root': settings.MEDIA_ROOT}),
 ]
